64634599596a8c01ef5bd95d100a89d7750297a7
betttyquu <absheng947@hotmail.com>
2019-05-06 19:07:51 +0000

    made a getAndPrintHTMLChunks.js

---
+++ getAndPrintHTMLchunks.js
@@ -0,0 +1,20 @@
+var https = require('https');    
+    var requestOptions = {
+        host: 'sytantris.github.io',
+        path: '/http-examples/step1.html'
+      };                           
+
+// function getAndPrintHTMLChunks () {
+
+    
+    https.get(requestOptions, function (response) {
+        response.on('data',function(data){
+          console.log(data.length);
+        });
+
+        response.on('end', function() {
+            console.log('Response stream complete.');
+        });
+    })
+  
+//   }
