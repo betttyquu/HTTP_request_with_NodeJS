ccd8adfab174c6c16b3ffd9e84eaf161208f9687
betttyquu <absheng947@hotmail.com>
2019-05-06 19:52:51 +0000

    implemented the function witha call back

--- print_in_callbacks.js
+++ print_in_callbacks.js
@@ -1,20 +1,22 @@
 var https = require('https');    
+// function getHTML (options, callback) {
     var requestOptions = {
         host: 'sytantris.github.io',
-        path: '/http-examples/step1.html'
-      };                           
-
-function getAndPrintHTMLChunks () {
-
-    
+        path: '/http-examples/step4.html'
+      };
     https.get(requestOptions, function (response) {
         response.on('data',function(data){
-          console.log(data + '\n');
+            console.log(data +=  '\n') + requestOptions + printHTML;   
         });
 
         response.on('end', function() {
             console.log('Response stream complete.');
         });
     })
-  
+
+  function printHTML (html) {
+    console.log(html);
   }
+// }
+  
+
