089d2a036f3c9d494132802c14f662986d04963c
betttyquu <absheng947@hotmail.com>
2019-05-06 19:16:19 +0000

    created getAndPrintHTMLChunks.js

--- getAndPrintHTMLchunks.js
+++ getAndPrintHTMLchunks.js
@@ -9,7 +9,7 @@ var https = require('https');
     
     https.get(requestOptions, function (response) {
         response.on('data',function(data){
-          console.log(data.length);
+          console.log(data + '\n');
         });
 
         response.on('end', function() {
