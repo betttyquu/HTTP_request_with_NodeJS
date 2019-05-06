739f77aff2bb550000c104598eaad38ab15febaf
betttyquu <42622870+betttyquu@users.noreply.github.com>
2019-05-06 14:48:22 -0700

    Update print_in_callbacks.js

--- print_in_callbacks.js
+++ print_in_callbacks.js
@@ -1,22 +1,23 @@
 var https = require('https');    
-// function getHTML (options, callback) {
-    var requestOptions = {
-        host: 'sytantris.github.io',
-        path: '/http-examples/step4.html'
-      };
-    https.get(requestOptions, function (response) {
-        response.on('data',function(data){
-            console.log(data +=  '\n') + requestOptions + printHTML;   
-        });
+function getHTML (options, callback) {
+  var result = ''
+  https.get(options, function (response) {
+    response.on('data',function(data){
+      result += data   
+    });
+    response.on('end', function() {
+      callback(result)
+    console.log('Response stream complete.');
+    });
+  })
 
-        response.on('end', function() {
-            console.log('Response stream complete.');
-        });
-    })
-
-  function printHTML (html) {
-    console.log(html);
-  }
-// }
-  
+}
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step4.html'
+};
+function printHTML (html) {
+  console.log(html);
+}
+getHTML(requestOptions, printHTML);
 
